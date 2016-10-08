26285c827850527fda3ce651a1e6688014843601
jsannyjr <jsanny1@lion.lmu.edu>
2016-10-07 23:33:31 -0700

    Started Character Classes
    Made some base classes for the characters and their stats. Preliminary

---
+++ Impossible3-Draft (Sandbox)/Assets/Base Characters/CharacterClasses.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterClasses{
+	//Class Type and Description
+	private string characterClassName;
+	private string characterClassDescription;
+
+	//Stats
+	private int health; 
+	private int movementCooldownTimer;
+	private int actionCoooldownTimer;
+	private int attackStrength;
+	private int armorValue;
+
+	public string CharacterClassName{ //Returns or sets class name
+		get{ 
+			return characterClassName; 
+		}
+		set{
+			characterClassName = value; 
+		}
+	}
+	public string CharacterClassDescription{
+		get{ 
+			return characterClassDescription; 
+		}
+		set{
+			characterClassName = value; 
+		}
+	}
+	public int Health{
+		get{ 
+			return health; 
+		}
+		set{
+			health = value; 
+		}
+	}
+	public int MovementCooldownTimer{
+		get{ 
+			return movementCooldownTimer; 
+		}
+		set{
+			movementCooldownTimer = value; 
+		}
+	}
+	public int ActionCooldownTimer{
+		get{ 
+			return actionCoooldownTimer; 
+		}
+		set{
+			actionCoooldownTimer = value; 
+		}
+	}
+	public int AttackStrength{
+		get{ 
+			return attackStrength; 
+		}
+		set{
+			attackStrength = value; 
+		}
+	}
+	public int ArmorValue{
+		get{ 
+			return armorValue; 
+		}
+		set{
+			armorValue = value; 
+		}
+	}
+}
