4730e78773135bde7dd49c57f7f8d79bbff0bb56
jsannyjr <jsanny1@lion.lmu.edu>
2016-10-08 16:20:33 -0700

    started ability trees and classes.
    Made some consctructor abilities and get methods.

--- Impossible3-Draft (Sandbox)/Assets/Ability System/Ability.cs
+++ Impossible3-Draft (Sandbox)/Assets/Ability System/Ability.cs
@@ -15,20 +15,59 @@ public class Ability : AbilityBehaviors {
 	private int cooldown; //ability cooldown
 	private List<AbilityBehaviors> affectedStats; //list of affected stats
 	private GameObject particleEffect; //needs assigned when we create the ability 
+	private int range; //range of ability
+	private bool aoe; //area of effect ability
+	private bool singleTarget; // single target ability 
 
-	public Ability(string abilityName, string abilityDescription, Sprite abilityIcon, List<AbilityBehaviors> abilityBehaviors, int abilityCooldown){
+	public Ability(string abilityName, string abilityDescription, Sprite abilityIcon, List<AbilityBehaviors> abilityBehaviors, GameObject abilityParticle, bool abilityRequiresTarget, int abilityRange){ //basic ability / basic attack
+		name = abilityName; 
+		description = abilityDescription;
+		icon = abilityIcon;
+		behaviors = new List<AbilityBehaviors> ();
+		behaviors = abilityBehaviors;
+		cooldown = 0; 
+		requiresTarget = abilityRequiresTarget; 
+		canCastOnSelf = false;
+		canCastOnParty = false; 
+		range = abilityRange;
+		aoe = false; 
+		singleTarget = false; 
+
+	}
+	public Ability(string abilityName, string abilityDescription, Sprite abilityIcon, List<AbilityBehaviors> abilityBehaviors, int abilityCooldown, GameObject particle, bool abilityRequiresTarget, int abilityRange, bool abilitySingleTarget){ //single target ability
 		name = abilityName;
+		description = abilityDescription; 
 		icon = abilityIcon;
 		behaviors = new List<AbilityBehaviors> ();
 		behaviors = abilityBehaviors;
 		cooldown = abilityCooldown; 
-		requiresTarget = false; 
+		requiresTarget = abilityRequiresTarget; 
 		canCastOnSelf = false;
 		canCastOnParty = false; 
-		description = "Default" 
-
+		range = abilityRange; 
+		aoe = false;
+		singleTarget = abilitySingleTarget;
 	}
 
-
+	public string AbilityName{
+		get{
+			return name; 
+		}
+	}
+	public string AbilityDescription{
+		get{
+			return description; 
+		}
+	}
+	public string AbilityIcon{
+		get{
+			return icon; 
+		}
+	}
+	public int AbilityCooldown{
+		get{
+			return cooldown; 
+		}
+	}
 
 }
