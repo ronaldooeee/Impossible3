3a493666aaa571b117d64f7e0d493f7eb09f575b
jsannyjr <jsanny1@lion.lmu.edu>
2016-10-08 00:35:05 -0700

    Made the other two base classes
    Added an Archer and Mage Class to the assests

---
+++ Impossible3-Draft (Sandbox)/Assets/Base Characters/BaseMageClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMageClass : CharacterClasses {
+
+	public void MageClass(){ //Mage Class
+		CharacterClassName = "Mage";
+		CharacterClassName = "A practicioner of the arcane arts.";
+		Health = 30;
+		MovementCooldownTimer = 5;
+		MovementRange = 5;
+		ActionCooldownTimer = 5;
+		AttackStrength = 10;
+		ArmorValue = 10;
+	}
+}
---
+++ Impossible3-Draft (Sandbox)/Assets/Base Characters/BaseRangerClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseRamgerClass : CharacterClasses {
+
+	public void RangerClass(){ //Ranger Class
+		CharacterClassName = "Ranger";
+		CharacterClassName = "A man of the North, ready to use his bow and daggers to defeat his enemies.";
+		Health = 40;
+		MovementCooldownTimer = 5;
+		MovementRange = 5;
+		ActionCooldownTimer = 5;
+		AttackStrength = 10;
+		ArmorValue = 10;
+	}
+}
