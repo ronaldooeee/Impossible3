bc74099f8cb15a5de40ff97a8a9131e5b5c820b8
Nicholas Soffa <nsoffa@lion.lmu.edu>
2016-10-16 00:25:14 -0700

    Added Health Sliders and Basic Health System to Units

---
+++ Impossible3/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthSystem : MonoBehaviour {
+
+	public int startingHealth = 100;
+	private int currentHealth;
+
+	public Slider healthSlider;
+	public Image fillImage;
+	public Color fullHealth = Color.green;
+	public Color noHealth = Color.red;
+
+	private void OnEnable ()
+	{
+		currentHealth = startingHealth;
+
+		SetHealthUI ();
+	}
+
+	public void TakeDamage (int amount)
+	{
+		currentHealth -= amount;
+
+		SetHealthUI ();
+
+		if (currentHealth <= 0) 
+		{
+			Destroy (this);
+		}
+	}
+
+	private void SetHealthUI()
+	{
+		healthSlider.value = currentHealth;
+
+		fillImage.color = Color.Lerp (noHealth, fullHealth, currentHealth / startingHealth);
+	}
+}
