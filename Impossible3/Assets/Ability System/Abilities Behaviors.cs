19501ad94b86b77a70bbf4efd0847d28c3d8b18c
jsannyjr <jsanny1@lion.lmu.edu>
2016-10-23 20:20:33 -0700

    Added back old files

---
+++ Impossible3/Assets/Ability System/Abilities Behaviors.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityBehaviors{
+
+	private BasicObjectInformation objectInfo;
+	private BehaviorStartTimes startTime;
+
+	public AbilityBehaviors(BasicObjectInformation basicInfo, BehaviorStartTimes sTime){
+		objectInfo = basicInfo;
+		startTime = sTime;
+	}
+
+	public enum BehaviorStartTimes{ //effects at the start, middle, and end 
+		Beginning,
+		Middle, 
+		End
+	}
+
+	public virtual void PerformBehavior(){
+	}
+
+	public BasicObjectInformation AbilityBehaviorInfo{
+		get{
+			return objectInfo;
+		}
+	}
+	public BehaviorStartTimes AbilityBehaviorStartTime{
+		get { 
+			return startTime;
+		}
+	}
+}
