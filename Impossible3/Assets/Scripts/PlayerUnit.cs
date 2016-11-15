612b95fa63a16a17a0ea1f4c81fa3c97c29529a8
jsannyjr <jsanny1@lion.lmu.edu>
2016-11-14 22:27:04 -0800

    starting ability ranges
    going to figure out how to make this diff for multiple ranges

--- Impossible3/Assets/Scripts/PlayerUnit.cs
+++ Impossible3/Assets/Scripts/PlayerUnit.cs
@@ -246,4 +246,120 @@ public class PlayerUnit : Unit
 
 		return aSpaces;
 	}
+	public bool[,] PossibleAbilities ()
+	{
+		//Get a multidimensional grid of booleans representing spaces it can move to
+		bool[,] abilSpaces = new bool[BoardManager.mapSize, BoardManager.mapSize];
+		Unit a, a2;
+
+		//Straight
+		//If not against very top of board, do this for one square ahead
+		if (CurrentY != BoardManager.mapSize - 1) 
+		{
+			a = BoardManager.Instance.Units [CurrentX, CurrentY + 1];
+			//if (a == null)
+			abilSpaces [CurrentX, CurrentY + 1] = true;
+
+			if (CurrentY != BoardManager.mapSize - 2) 
+			{
+				a2 = BoardManager.Instance.Units [CurrentX, CurrentY + 2];
+				//if (a2 == null)
+				abilSpaces [CurrentX, CurrentY + 2] = true;
+			}
+		}
+
+		//Back
+		if (CurrentY != 0) 
+		{
+			a = BoardManager.Instance.Units [CurrentX, CurrentY - 1];
+			//if (a == null)
+			abilSpaces [CurrentX, CurrentY - 1] = true;
+			if (CurrentY != 1) 
+			{
+				a2 = BoardManager.Instance.Units [CurrentX, CurrentY - 2];
+				//if (a2 == null)
+				abilSpaces [CurrentX, CurrentY - 2] = true;
+			}
+		}
+
+		//Left
+		if (CurrentX != 0) 
+		{
+			a = BoardManager.Instance.Units [CurrentX - 1, CurrentY];
+			//if (a == null)
+			abilSpaces [CurrentX - 1, CurrentY] = true;
+			if (CurrentX != 1) 
+			{
+				a2 = BoardManager.Instance.Units [CurrentX - 2, CurrentY];
+				//if (a2 == null)
+				abilSpaces [CurrentX - 2, CurrentY] = true;
+			}
+		}
+
+		//Right
+
+		if (CurrentX != BoardManager.mapSize - 1) 
+		{
+			a = BoardManager.Instance.Units [CurrentX + 1, CurrentY];
+			if (a == null)
+				abilSpaces [CurrentX + 1, CurrentY] = true;
+			if (CurrentX != BoardManager.mapSize - 2) 
+			{
+				a2 = BoardManager.Instance.Units [CurrentX + 2, CurrentY];
+				//if (a2 == null)
+				abilSpaces [CurrentX + 2, CurrentY] = true;
+			}
+		}
+
+		//Diag Front Left
+		//If not against left side of board or at very top of board, Do this
+		if (CurrentX != 0) 
+		{
+			if (CurrentY != BoardManager.mapSize - 1) {
+				//Check BoardObject at that x,y value
+				a = BoardManager.Instance.Units [CurrentX - 1, CurrentY + 1];
+				//If its open, be able to move there
+				//if (a == null)
+				abilSpaces [CurrentX - 1, CurrentY + 1] = true;
+			}
+		}
+
+		//Diag Front Right
+		//If not against right side of board or at very top of board, Do this
+		if (CurrentX != BoardManager.mapSize - 1) 
+		{
+			if (CurrentY != BoardManager.mapSize - 1) {
+				a = BoardManager.Instance.Units [CurrentX + 1, CurrentY + 1];
+				//if (a == null)
+				abilSpaces [CurrentX + 1, CurrentY + 1] = true;
+			}
+		}
+
+		//Diag Back Left
+		if (CurrentX != 0) 
+		{
+			if (CurrentY != 0) {
+				//Check BoardObject at that x,y value
+				a = BoardManager.Instance.Units [CurrentX - 1, CurrentY - 1];
+				//If its open, be able to move there
+				//if (a == null)
+				abilSpaces [CurrentX - 1, CurrentY - 1] = true;
+			}
+		}
+
+		//Diag Back Right
+		if (CurrentX != BoardManager.mapSize - 1) 
+		{
+			if (CurrentY != 0) {
+				//Check BoardObject at that x,y value
+				a = BoardManager.Instance.Units [CurrentX + 1, CurrentY - 1];
+				//If its open, be able to move there
+				//if (a == null)
+				abilSpaces [CurrentX + 1, CurrentY - 1] = true;
+			}
+		}
+
+		return abilSpaces;
+	}
+
 }
