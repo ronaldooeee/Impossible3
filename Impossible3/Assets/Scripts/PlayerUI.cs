1c9c254fc790eae869bc3ef1b24006e2edf4e88d
Flanders Lorton <florton@lion.lmu.edu>
2016-11-16 20:16:21 -0800

    UI hotfixes

--- Impossible3/Assets/Scripts/PlayerUI.cs
+++ Impossible3/Assets/Scripts/PlayerUI.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class PlayerUI : MonoBehaviour {
+public class PlayerUI : MonoBehaviour
+{
     public List<GameObject> unitPrefabs;
     private List<GameObject> UIs;
     private List<GameObject> Units;
@@ -11,43 +12,44 @@ public class PlayerUI : MonoBehaviour {
     public GameObject mainUI;
 
 
-    void Start () {
-        float width = mainCanvas.transform.localScale.x;
-        float height = mainCanvas.transform.localScale.y;
-        
+    void Start()
+    {
+
         UIs = new List<GameObject>();
-        GameObject player1UI = Instantiate(mainUI, new Vector3(0,0,0), Camera.main.transform.rotation) as GameObject;
+        GameObject player1UI = Instantiate(mainUI, new Vector3(0, 0, 0), Camera.main.transform.rotation) as GameObject;
         player1UI.transform.parent = mainCanvas.transform;
-        player1UI.transform.localScale = new Vector3(.2f,.2f,.2f);
-        player1UI.GetComponentsInChildren<RectTransform>()[0].anchorMin = new Vector2(0,1);
-        player1UI.GetComponentsInChildren<RectTransform>()[0].anchorMax = new Vector2(0,1);
-        player1UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(-350,190,0);
+        player1UI.transform.localScale = new Vector3(.2f, .2f, .2f);
+        player1UI.GetComponentsInChildren<RectTransform>()[0].anchorMin = new Vector2(0, 1);
+        player1UI.GetComponentsInChildren<RectTransform>()[0].anchorMax = new Vector2(0, 1);
+        player1UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(0 - (float)(Screen.width / 2.5), 0 + (float)(Screen.height / 3), 0);
+        //player1UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(-350,190,0);
 
         GameObject player2UI = Instantiate(mainUI, new Vector3(0, 0, 0), Camera.main.transform.rotation) as GameObject;
         player2UI.transform.parent = mainCanvas.transform;
         player2UI.transform.localScale = new Vector3(.2f, .2f, .2f);
         player2UI.GetComponentsInChildren<RectTransform>()[0].anchorMin = new Vector2(0, 1);
         player2UI.GetComponentsInChildren<RectTransform>()[0].anchorMax = new Vector2(0, 1);
-        player2UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(-350, 90, 0);
+        player2UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(0 - (float)(Screen.width / 2.5), 0 + (float)(Screen.height / 7.5), 0);
 
         GameObject player3UI = Instantiate(mainUI, new Vector3(0, 0, 0), Camera.main.transform.rotation) as GameObject;
         player3UI.transform.parent = mainCanvas.transform;
         player3UI.transform.localScale = new Vector3(.2f, .2f, .2f);
         player3UI.GetComponentsInChildren<RectTransform>()[0].anchorMin = new Vector2(0, 1);
         player3UI.GetComponentsInChildren<RectTransform>()[0].anchorMax = new Vector2(0, 1);
-        player3UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(-350, -10, 0);
+        player3UI.GetComponentsInChildren<RectTransform>()[0].localPosition = new Vector3(0 - (float)(Screen.width / 2.5), 0 - (float)(Screen.height / 14), 0);
 
         UIs.Add(player1UI);
         UIs.Add(player2UI);
         UIs.Add(player3UI);
     }
 
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Units = BoardManager.playerUnits;
-        for (int i = 0; i<UIs.Count; i++)
+        for (int i = 0; i < UIs.Count; i++)
         {
             Image[] bars = UIs[i].GetComponentsInChildren<Image>();
             Image[] bars0 = bars[1].GetComponents<Image>();
@@ -58,7 +60,10 @@ public class PlayerUI : MonoBehaviour {
                 //HP Bar UI
                 HealthSystem playerHP = Units[i].GetComponents<HealthSystem>()[0];
                 float healthratio = (float)playerHP.currentHealth / (float)playerHP.startingHealth;
-                bars0[0].color = Color.Lerp(playerHP.noHealth, playerHP.fullHealth, playerHP.currentHealth / playerHP.startingHealth);
+                if (healthratio >= .66) { bars0[0].color = new Color(0,1,0,1); }
+                else if (healthratio >= .33) { bars0[0].color = new Color(1, 0.92f, 0.016f, 1); }
+                else { bars0[0].color = new Color(1, 0, 0, 1); }
+                //bars0[0].color = Color.Lerp(playerHP.noHealth, playerHP.fullHealth, playerHP.currentHealth / playerHP.startingHealth);
                 bars0[0].transform.localScale = new Vector3((float)7 * healthratio, bars0[0].transform.localScale.y, bars0[0].transform.localScale.z);
                 //bars0[0].transform.position = new Vector3(0, bars0[0].transform.position.y, bars0[0].transform.position.z);
 
@@ -67,7 +72,7 @@ public class PlayerUI : MonoBehaviour {
                 float maxMoveCooldown = Units[i].GetComponents<PlayerUnit>()[0].cooldownMoveSeconds;
                 if (playerMoveCooldown >= 0)
                 {
-                    bars1[0].transform.localScale = new Vector3((float)7 * (1-(playerMoveCooldown/maxMoveCooldown)), bars1[0].transform.localScale.y, bars1[0].transform.localScale.z);
+                    bars1[0].transform.localScale = new Vector3((float)7 * (1 - (playerMoveCooldown / maxMoveCooldown)), bars1[0].transform.localScale.y, bars1[0].transform.localScale.z);
                 }
 
                 //Attack Cooldown UI
@@ -81,8 +86,8 @@ public class PlayerUI : MonoBehaviour {
             }
             //catch
             //{
-                //Temp workaround for when players are deleted so unity doesnt throw 1 million errors
+            //Temp workaround for when players are deleted so unity doesnt throw 1 million errors
             //}
         }
-	}
+    }
 }
