63c77610495f26fd7bca28af5e193f6629358e89
Nick Soffa <nsoffa@lion.lmu.edu>
2016-11-17 10:01:24 -0800

    Camera Movement
    Move the Camera with WASD

---
+++ Impossible3/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	//private int boundary = 50;
+	private int speed = 10;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+	}
+}
